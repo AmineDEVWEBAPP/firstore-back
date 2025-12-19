9ede29ac15218931371ee32ca680b69beaf06bb7
root <root@localhost>
2025-12-20 00:01:51 +0100

    add middleware to profile

--- v1/routes/profiles.js
+++ v1/routes/profiles.js
@@ -1,11 +1,14 @@
 import express from "express";
 import { createProfile, getProfiles, updateProfile } from "../controller/profiles.js";
 import strictArgs from "../middlewares/strictArgs.js";
+import authJWT from "../middlewares/admin/authJWT.js";
 
 const router = express.Router();
 export default router
 
 router.get('/', getProfiles)
+
+router.use(authJWT)
 
 router.post('/',
     strictArgs({ 'accountId': 'number', 'name': 'string', 'pinCode': 'number', 'paymentUrl': 'string' }),
