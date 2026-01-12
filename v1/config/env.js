586886650f6c0be3c2ded16df6b6e58cadb024ab
root <root@localhost>
2026-01-12 06:45:25 +0100

    add type to package

--- v1/config/env.js
+++ v1/config/env.js
@@ -2,14 +2,14 @@ process.loadEnvFile('.env')
 
 const envName = process.env.ENV_NAME === 'production' ? 'prod' : 'dev'
 
-  const env={
-    'envName':envName,
-    'port':process.env.PORT,
-    'DB_HOST':process.env.DB_HOST,
-    'DB_USER':process.env.DB_USER,
-    'DB_PASSWORD':process.env.DB_PASSWORD,
-    'DB_NAME':process.env.DB_NAME,
-    'JWT_SECRET': process.env.JWT_SECRET
-  }
+const env = {
+  'envName': envName,
+  'port': process.env.PORT || 8080,
+  'DB_HOST': process.env.DB_HOST,
+  'DB_USER': process.env.DB_USER,
+  'DB_PASSWORD': process.env.DB_PASSWORD,
+  'DB_NAME': process.env.DB_NAME,
+  'JWT_SECRET': process.env.JWT_SECRET
+}
 
-  export default env
+export default env
